405c199aa87725be7140c89a8b3202c9b5fbcd1a
carrotten <vilte.cibi@gmail.com>
2023-08-27 00:00:19 +0200

    Update BossTwo.java
    Refurbishing of the old BossTwo.java.
    The changes are the same as in BossOne but I chose to keep it because it makes the textures more manageable.
    
    Changes:
    1. A lot of variables got moved to the parent class and are interacted with from there
    2. A lot of new variables to implement animations
    3. method move() got updated and moved to the parent class
    4. shoot() method has been finally implemented
    5. javadoc-ability and general readability

--- src/Boss/BossTwo.java
+++ src/Boss/BossTwo.java
@@ -1,134 +1,206 @@
 package Boss;
 
-import org.jsfml.graphics.RectangleShape;
+import org.jsfml.graphics.Image;
+import org.jsfml.graphics.IntRect;
+import org.jsfml.graphics.Texture;
+import org.jsfml.graphics.TextureCreationException;
 
-import java.util.Random;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
- * @author: Yujie Chen
- * @edited: Viltene Cibirkaite
- * @date: 21-02-12
- * @version: 1.2.0
+ * @author: Yujie
+ * @author: Viltene
+ * @version: 21-02-12, last updated on 23-08-26
+ * This class extends Boss class and is responsible for
+ * the boss that appears in the mannor stage
  */
-public class BossTwo extends Boss {
-    private int BossOneLife = 4;
-    private float BossOneDamage = 100;
-    private float BossOnePositionX = 850;
-    private float BossOnePositionY = 360;
-    private boolean boundary = false;
-    private int decide = 0;
-    Random random = new Random();
-    private RectangleShape boss;
-    // private RectangleShape bullet;
-    // private Player player;
+public class BossTwo extends Boss 
+{
+    //animation variables initialisation
+    private int idleTracker;
+    private int hurtingAnimationTracker;
+    private Texture iR;
+    private Image idleRight;
+    private Texture iL;
+    private Image idleLeft;
+    private Image shootSheet;
+    private Texture shootingAnimation;
+    private Image hurtingRight;
+    private Image hurtingLeft;
+    private Texture hR;
+    private Texture hL;
+    private int currentShootingFrame;
+    private int animationPacer;
 
-    public BossTwo(float bossOneLife, float bossOneDamage, float bossOnePositionX, float bossOnePositionY) {
-        BossOneLife = (int) bossOneLife;
-        BossOneDamage = bossOneDamage;
-        BossOnePositionX = bossOnePositionX;
-        BossOnePositionY = bossOnePositionY;
-        boss = createABoss(100, 100, bossOnePositionX, bossOnePositionY);
-    }
+    public BossTwo() 
+    {
+        /* Initialising basic parameters */
+        super.widthOfSprite = 128;
+        super.heightOfSprite = 128;
+        super.boss = createABoss(widthOfSprite, heightOfSprite, positionX, positionY);
 
-    public void move() {
-        int r = random.nextInt(3);
-        if (r == 0 || r == 2) {
-            if (decide == 0) {
-                if (BossOnePositionX < 480) {
-                    BossOnePositionX += 10;
-                    returnedBlood();
-                } else if (BossOnePositionX >= 480 && BossOnePositionX <= 1150) {
-                    BossOnePositionX += 10;
-                } else if (BossOnePositionX > 1150) {
-                    BossOnePositionX -= 10;
-                    returnedBlood();
-                    decide = 1;
-                }
-            } else if (decide == 1) {
-                if (BossOnePositionX < 480) {
-                    BossOnePositionX += 10;
-                    returnedBlood();
-                    decide = 0;
-                } else if (BossOnePositionX >= 480 && BossOnePositionX <= 1150) {
-                    BossOnePositionX -= 10;
-                }
-            }
-        } else if (r == 1) {
-            if (decide == 0) {
-                if (BossOnePositionY < 60) {
-                    BossOnePositionY += 10;
-                    returnedBlood();
-                } else if (BossOnePositionY > 660) {
-                    BossOnePositionY -= 10;
-                    returnedBlood();
-                    decide = 1;
-                } else if (BossOnePositionY >= 60 && BossOnePositionY <= 660) {
-                    BossOnePositionY += 10;
-                }
-            } else if (decide == 1) {
-                if (BossOnePositionY < 60) {
-                    BossOnePositionY += 10;
-                    returnedBlood();
-                    decide = 0;
-                } else if (BossOnePositionY > 660) {
-                    BossOnePositionY -= 10;
-                    returnedBlood();
-                } else if (BossOnePositionY >= 60 && BossOnePositionY <= 660) {
-                    BossOnePositionY -= 10;
-                }
-            }
-            shoot(BossOnePositionX, BossOnePositionY);
-
+        /* Variables associated with animations */
+        idleTracker = 0;
+        hurtingAnimationTracker = 0;
+        animationPacer = 0;
+        currentShootingFrame = 640;
+        /* Images */
+        idleRight = new Image();
+        idleLeft = new Image();
+        shootSheet = new Image();
+        hurtingRight = new Image();
+        hurtingLeft = new Image();
+        try {
+            idleRight.loadFromFile(Paths.get("ArtAssets/Countess_Vampire/IdleR.png"));
+            idleLeft.loadFromFile(Paths.get("ArtAssets/Countess_Vampire/IdleL.png"));
+            shootSheet.loadFromFile(Paths.get("ArtAssets/Countess_Vampire/Attack.png"));
+            hurtingRight.loadFromFile(Paths.get("ArtAssets/Countess_Vampire/HurtR.png"));
+            hurtingLeft.loadFromFile(Paths.get("ArtAssets/Countess_Vampire/HurtL.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        boss.setPosition(BossOnePositionX, BossOnePositionY);
-
-    }
-
-    public void shoot(float x, float y) {
-
-    }
-
-    public void returnedBlood() {
-        if (BossOneLife < 3) {
-            BossOneLife += 1;
+        /* Textures */
+        iR = new Texture();
+        iL = new Texture();
+        shootingAnimation = new Texture();
+        hR = new Texture();
+        hL = new Texture();
+        try{
+            iL.loadFromImage(idleLeft, new IntRect(0, 0, widthOfSprite, heightOfSprite));
+            shootingAnimation.loadFromImage(shootSheet, new IntRect(currentShootingFrame, 0, widthOfSprite, heightOfSprite));
+            hR.loadFromImage(hurtingRight, new IntRect(0, 0, widthOfSprite, heightOfSprite));
+            hL.loadFromImage(hurtingLeft, new IntRect(256-widthOfSprite, 0, widthOfSprite, heightOfSprite));
+        }catch (TextureCreationException tce){
+            tce.printStackTrace();
         }
+
+        super.boss.setTexture(iL); //set primary texture
+        super.right = false;
     }
 
-    public void setBossTwoLife(float bossOneLife) {
-        BossOneLife = (int) bossOneLife;
-    }
-
-    public void setBossTwoDamage(double bossOneDamage) {
-        BossOneDamage = (int) bossOneDamage;
-    }
-
-    public void setBossTwoPositionX(float bossOnePositionX) {
-        BossOnePositionX = bossOnePositionX;
-    }
-
-    public void setBossTwoPositionY(float bossOnePositionY) {
-        BossOnePositionY = bossOnePositionY;
-    }
-
-    public double getBossTwoLife() {
-        return BossOneLife;
-    }
-
-    public double getBossTwoDamage() {
-        return BossOneDamage;
-    }
-
-    public float getBossTwoPositionX() {
-        return BossOnePositionX;
-    }
-
-    public float getBossTwoPositionY() {
-        return BossOnePositionY;
-    }
-
+    /**
+     * Implements the shooting mechanic and animation
+    */
     @Override
-    public void move(int playerX, int playerY) {
-        // TODO Auto-generated method stub
+    public void shoot() {
+        if(!shooting && !hurting) //if not shooting or hurting then could shoot
+        {
+            int rand = random.nextInt(1000);
+            if (rand >= 980)
+            {
+                super.shooting = true; // decided to shoot
+                super.boss.setTexture(shootingAnimation);
+            }
+        }
+        
+        /* Shooting animation frame to frame */
+        if(animationPacer==2 && shooting)
+        {
+            if(currentShootingFrame - widthOfSprite >= 0 - widthOfSprite)
+            {
+                try{
+                    shootingAnimation.loadFromImage(shootSheet, new IntRect(currentShootingFrame, 0, widthOfSprite, heightOfSprite));
+                }catch (TextureCreationException tce){
+                tce.printStackTrace();}
+                super.boss.setTexture(shootingAnimation);
+                currentShootingFrame = currentShootingFrame - widthOfSprite;
+                animationPacer = 0;
+            }
+            else{
+                super.shooting = false;
+                animationPacer = 0;
+                currentShootingFrame = 640;
+                try{
+                    shootingAnimation.loadFromImage(shootSheet, new IntRect(currentShootingFrame, 0, widthOfSprite, heightOfSprite));
+                }catch (TextureCreationException tce){
+                tce.printStackTrace();}
+            }
+        }
+
+        if(shooting)
+        {
+            animationPacer++;
+        }    
+    }
+
+    /**
+     * Implementation of idle/floating animation
+     */
+    @Override
+    public void animation (){
+        if (right)
+        {
+            try{
+                iR.loadFromImage(idleRight, new IntRect(0+idleTracker*widthOfSprite, 0, widthOfSprite, heightOfSprite));
+            }catch (TextureCreationException tce){
+                tce.printStackTrace();}
+            super.boss.setTexture(iR);
+        }
+        else
+        {
+            try{
+                iL.loadFromImage(idleLeft, new IntRect(0+idleTracker*widthOfSprite, 0, widthOfSprite, heightOfSprite));
+            }catch (TextureCreationException tce){
+                tce.printStackTrace();}
+            super.boss.setTexture(iL);
+        }
+        if(idleTracker==4)
+        {
+            idleTracker = 0;
+        }
+        else
+        {
+            idleTracker++;
+        }
+    }
+
+    /**
+     * Getter of shooting state
+     * @return true = currently shooting || false = currently not shooting
+     */
+    @Override
+    public boolean getShooting()
+    {
+        boolean tf = false;
+        if(currentShootingFrame == 128 && animationPacer==2)
+        {
+            tf = true;
+        }
+        return tf;
+    }
+
+    /**
+     * Implementation of the hurting animation
+     */
+    @Override
+    public void hurtingAnimation() 
+    {
+        if (right && hurtingAnimationTracker < 2)
+        {
+            try{
+                hR.loadFromImage(hurtingRight, new IntRect(0+hurtingAnimationTracker*widthOfSprite, 0, widthOfSprite, heightOfSprite));
+            }catch (TextureCreationException tce){
+                tce.printStackTrace();}
+            super.boss.setTexture(hR);
+            hurtingAnimationTracker++;
+        }
+        else if (hurtingAnimationTracker < 2)
+        {
+            try{
+                hL.loadFromImage(hurtingLeft, new IntRect((256-widthOfSprite)-widthOfSprite*hurtingAnimationTracker, 0, widthOfSprite, heightOfSprite));
+            }catch (TextureCreationException tce){
+                tce.printStackTrace();}
+            super.boss.setTexture(hL);
+            hurtingAnimationTracker++;
+        }
+        else
+        {
+            super.hurting = false;
+            hurtingAnimationTracker = 0;
+        }
 
     }
+
+    
 }
