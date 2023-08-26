ba67d38ee9f097e5fb6f88682e3b41321aaca05a
carrotten <vilte.cibi@gmail.com>
2023-08-27 00:08:41 +0200

    Update Level.java
    Not many changes. Mostly comments

--- src/Levels/Level.java
+++ src/Levels/Level.java
@@ -7,20 +7,36 @@ import org.jsfml.graphics.TextureCreationException;
 
 import java.util.Scanner;
 
+/**
+ * @author: Viltene
+ * It is called by LevelSet which passes down the scanner and appropriate art assets to create platforms.
+ * 
+ * This part of the Level package is concerned with putting together multiple platforms to make a level.
+ * This is done by calling Platform class for every read data about platforms and 
+ * applying textures to the returned represenattions.
+ * 
+ * There is a .txt explaining how levels are made in Level resources 
+ * @see <a href= "structureOfTheLevelFiles.txt"> More information on types </a>  
+ */
 public class Level
 {
+    // Primary initialisation
     int numOfPlatforms = 0;
     public Platform platforms[];
     Texture textures[] = new Texture[8];
     Image image[] = new Image[8];
-
+    /**
+     * Constructor for level
+     * @param s Scanner initialised in LevelSet
+     * @param im array of 8 of images that represent the different type of platforms 
+     */
     public Level(Scanner s, Image im[])
     {
         for(int i = 0; i < 8; i++)
         {
            image[i] = new Image();
            image[i] = im[i];
-
+           //making textures out of images
            textures[i] = new Texture();
            try{
                textures[i].loadFromImage(image[i]);
@@ -29,10 +45,14 @@ public class Level
                ex.printStackTrace();
            }
         }
-
+        //creates platforms
         declarePlatforms(s);
     }
 
+    /**
+     * Reads the platform file and establishes Platforms
+     * @param scanner Scanner
+     */
     private void declarePlatforms(Scanner scanner)
     {
         int l, h, x, y, t, d, movementx, movementy, mr;
@@ -51,82 +71,85 @@ public class Level
                 movementy = scanner.nextInt();
                 mr = scanner.nextInt();
                 platforms[i] = new Platform(l, h, x, y, t, d, isMoving, movementx, movementy, mr);
-                System.out.print(l + " " + h + " " + x + " " + y + " " + t + " " + isMoving + " " + movementx + " " + movementy + " " + mr + "\n");
             }
             setPlatformColors();
     }
 
+    /**
+     * Getter of the platform set
+     *  @return platforms - Platform type array
+     */
     public Platform[] getPlatforms ()
     {
         return platforms;
     }
 
+    /**
+     * Sets appropriate textures depending on the type
+     * @see <a href= "structureOfTheLevelFiles.txt"> More information on types </a>  
+     */
     private void setPlatformColors ()
     {
         for(int i = 0; i < numOfPlatforms; i++)
         {
-
+            // Type 0 - Spikes
             if(platforms[i].getPlatformType() == 0)
             {
+                // 1 - up 
                 if(platforms[i].getDirection() == 1)
                 {
                     platforms[i].setPlatformTexture(textures[0]);
                 }
 
+                // 2 - left
                 if(platforms[i].getDirection() == 2)
                 {
                     platforms[i].setPlatformTexture(textures[1]);
                 }
 
+                // 3 - down
                 if(platforms[i].getDirection() == 3)
                 {
                     platforms[i].setPlatformTexture(textures[2]);
                 }
 
+                // 4 - right
                 if(platforms[i].getDirection() == 4)
                 {
                     platforms[i].setPlatformTexture(textures[3]);
                 }
             }
 
+            // Type 1 - platform to stand on 
             if(platforms[i].getPlatformType() == 1)
             {
-//                try {
-//                    textures[1].loadFromImage(image[1]);
-//                    textures[1].setRepeated(true);
-                    platforms[i].setPlatformTexture(textures[4]);
-//                } catch (TextureCreationException ex){
-//                    ex.printStackTrace();
-//                }
+                platforms[i].setPlatformTexture(textures[4]);
             }
 
+            // Type 2 - platform to climb
             if(platforms[i].getPlatformType() == 2) {
+                // 1 - right
                 if (platforms[i].getDirection() == 1) {
                     platforms[i].setPlatformTexture(textures[5]);
                 }
-
+                // 2 - left
                 if (platforms[i].getDirection() == 2) {
                     platforms[i].setPlatformTexture(textures[6]);
 
                 }
             }
 
+            // Type 3 - filler
             if(platforms[i].getPlatformType() == 3)
             {
-//                try {
-//                    textures[3].loadFromImage(image[3]);
-//                    textures[3].setRepeated(true);
-                    platforms[i].setPlatformTexture(textures[7]);
-//                } catch (TextureCreationException ex){
-//                    ex.printStackTrace();
-//                }
+                platforms[i].setPlatformTexture(textures[7]);
             }
 
+            // if decorations ever were implemented
             if(platforms[i].getPlatformType() == 4)
             {
                 platforms[i].setPlatformColor(Color.MAGENTA);
             }
         }
     }
-
 }
