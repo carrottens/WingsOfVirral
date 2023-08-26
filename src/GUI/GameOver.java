6bbb6bc3c9737b92e16002394a178e4ef0026f08
carrotten <vilte.cibi@gmail.com>
2023-08-27 00:03:51 +0200

    Update GameOver.java
    I have not made many changes to this. Just made it more readable.

--- src/GUI/GameOver.java
+++ src/GUI/GameOver.java
@@ -6,35 +6,44 @@ import org.jsfml.system.Vector2f;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+/**
+ * @author: Viltene
+ * This class is responsible for game over graphics
+ * It is used in GUI
+ */
 public class GameOver
 {
+    /* Parameters of the Game Over tablet */
     private Vector2f v = new Vector2f (450, 130);
     private RectangleShape gameOverTablet;
     private Image gameOverImage;
     private Texture gameOverTexture;
-    RenderWindow w;
 
-    public GameOver(RenderWindow window)
+    public GameOver()
     {
-        w = window;
+        /* Initialisation */
         gameOverTablet = new RectangleShape(v);
         gameOverImage = new Image();
         try {
             gameOverImage.loadFromFile(Paths.get("ArtAssets/game over.png"));
-        }catch(IOException ex) {
+        } catch(IOException ex) {
             ex.printStackTrace();
         }
-        try{
-           gameOverTexture = new Texture();
+        gameOverTexture = new Texture();
+        try {           
            gameOverTexture.loadFromImage(gameOverImage);
-        }catch (TextureCreationException tce)
-        {
+        } catch (TextureCreationException tce) {
             tce.printStackTrace();
         }
+
         gameOverTablet.setTexture(gameOverTexture);
-        gameOverTablet.setPosition(415, 295);
+        gameOverTablet.setPosition(415, 295); // center
     }
 
+    /**
+     * Getter of the Game Over tablet
+     * @return game over tablet
+     */
     public RectangleShape gameOverAnimation()
     {
         return gameOverTablet;
