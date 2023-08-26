4975c1232ada0e3f7e0115ffc74bbe431693824f
carrotten <vilte.cibi@gmail.com>
2023-08-27 00:20:46 +0200

    Update PlayerJumpingAnimation.java
    Just added some comments

--- src/Player/PlayerJumpingAnimation.java
+++ src/Player/PlayerJumpingAnimation.java
@@ -8,20 +8,36 @@ import org.jsfml.graphics.TextureCreationException;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+/**
+ * @author: Viltene
+ * This class is responsible for animation of jumping.
+ * Implemented in the Player class
+*/
 public class PlayerJumpingAnimation
 {
-    Image jumpingRightCycle = new Image();
-    Texture jumpingRight = new Texture();
-    Image jumpingLeftCycle = new Image();
-    Texture jumpingLeft = new Texture();
-    IntRect sizeOfSpriteSheetFrame = new IntRect(0, 0, 34, 51);
+    /* Images */
+    private Image jumpingRightCycle = new Image();
+    private Image jumpingLeftCycle = new Image();
+
+    /* Textures */
+    private Texture jumpingRight = new Texture();
+    private Texture jumpingLeft = new Texture();
+
+    /* Sizes of the frame of the animation in the sprite sheet
+     * it was made by a group member and the width of the sprite 
+     * is varying
+     */
+    private IntRect sizeOfSpriteSheetFrame = new IntRect(0, 0, 34, 51);
     private int jumpRX [] = new int[] {0, 34, 68, 102, 136, 170, 204};
     private int jumpLX [] = new int[] {204, 170, 136, 102, 68, 34, 0};
-    int trackerL = 0;
-    int trackerR = 0;
+    
+    /* Frame trackers */
+    private int trackerL = 0;
+    private int trackerR = 0;
 
     public PlayerJumpingAnimation()
     {
+        /* Loading Images and Textures */
         try{
             jumpingRightCycle.loadFromFile(Paths.get("ArtAssets/jump2R.png"));
             jumpingRight.loadFromImage(jumpingRightCycle);
@@ -34,11 +50,19 @@ public class PlayerJumpingAnimation
         }
     }
 
+    /**
+     * Getter of the jumping to the right texture
+     * @return texture of jumping to the right
+     */
     public Texture getJumpRightTexture()
     {
         return jumpingRight;
     }
 
+    /**
+     * Getter of the appropriate jumping to the right animation frame  
+     * @return appropriate animation frame  of jumping to the right
+     */
     public IntRect getCurrentFrameJumpingRight()
     {
         trackerL = 0;
@@ -54,11 +78,19 @@ public class PlayerJumpingAnimation
         return sizeOfSpriteSheetFrame;
     }
 
+    /**
+     * Getter of the jumping to the left texture
+     * @return texture of jumping to the left
+     */
     public Texture getJumpLeftTexture()
     {
         return jumpingLeft;
     }
 
+    /**
+     * Getter of the appropriate jumping to the left animation frame 
+     * @return appropriate animation frame of jumping to the left
+     */
     public IntRect getCurrentFrameJumpingLeft()
     {
         trackerR = 0;
