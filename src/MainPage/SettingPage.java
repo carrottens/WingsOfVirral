797e37b0fe447fc7e16ab4b47b12afb88d3302ad
carrotten <vilte.cibi@gmail.com>
2023-08-27 00:13:17 +0200

    Update SettingPage.java
    Small aesthetic change in story text.
    Changes in comments and documentation.

--- src/MainPage/SettingPage.java
+++ src/MainPage/SettingPage.java
@@ -6,22 +6,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * The Setting page.
+ * The Setting page. Responsible for displaying the story of the 
+ * game and explaining the controls.
+ * Works on top of MainPage window.
+ * Allows getting back to the main menu (MainPage).
  *
- * @author: Yujie Chen
- * @date: 21-01-18
- * @version: 1.0.0
+ * @author: Yujie
+ * @author: Viltene
+ * @version: 21-01-18, last time updated on 23-08-18
+ * 
  */
-    public class SettingPage implements ActionListener {
+public class SettingPage implements ActionListener {
+
     private static JFrame startF = new JFrame(); // Create frame of Start.
     private JPanel startP = new JPanel();// Create page of Start.
     private JButton back = new JButton("BACK"); // Create back button.
     private JFrame menu = new JFrame();
     private JLabel title = new JLabel("<html><div style='text-align: center;'>" + "<br>" + "INSTRUCTIONS" + "</div></html>", SwingConstants.CENTER);
-    private JLabel instructions = new JLabel("<html><center>" + "A - walk left" + "<br>" + "D - walk right" + "<br>" + "W - jump" + "<br>" + "SHIFT - slow time", SwingConstants.CENTER);
-    private JLabel story = new JLabel("<html><center>" + "Help the brave young Lyra Cornelia Batterbee on her quest to complete her other wing!" + "<br>" + "Platform your way up to success while avoiding dangerous spikes and gaping holes in the ground. There are items to aid you in your journey but remember with great power comes great responsibility, so use them wisely" + "<html><center>");
+    private JLabel instructions = new JLabel("<html><center>" + "A - walk left" + "<br>" + "D - walk right" + "<br>" + "W - jump" + "<br>" + "SHIFT - slow time"+ "<br>" + "ESCAPE - exit" + "<html><center>", SwingConstants.CENTER);
+    private JLabel story = new JLabel("<html><center>" + "   Help the brave young Lyra Cornelia Batterbee on her quest to complete her other wing!" + "<br>" + "   Platform your way up to success while avoiding dangerous spikes and gaping holes in the ground." + "<br>" + "   There are items to aid you in your journey but remember with great power comes great responsibility, so use them wisely" + "<html><center>");
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+    /**
+     * Constructor for setting page
+     * @param mainMenu JFrame that has been initiallised in MainPage
+     */
     public SettingPage(JFrame mainMenu) {
         startP = new JPanel() {
             protected void paintComponent(Graphics g) {
@@ -66,9 +75,16 @@ import java.awt.event.ActionListener;
         story.setFont(new Font("Baskerville Old Face", Font.BOLD, 30));
         story.setForeground(new Color(255, 255, 255));
         story.setPreferredSize(new Dimension(509, 500));
+        story.setVisible(true);
         startP.add(story, BorderLayout.LINE_START); // Add the start button into page.
+
+        startF.setVisible(true); //refresh
     }
 
+    /**
+     * Implementation of the action listener interface.
+     *  @param e action event
+    */
     public void actionPerformed(ActionEvent e) {
         // The effect when the start button is clicked.
         if (e.getSource() == back) {
