725db0f9ec0cd6d97da3e68df098ee36e7c6a6af
carrotten <vilte.cibi@gmail.com>
2023-08-27 00:12:10 +0200

    Create Driver.java
    New class to "drive" the game

---
+++ src/MainPage/Driver.java
@@ -0,0 +1,17 @@
+package MainPage;
+
+/** 
+ * Authors of the whole game:
+ * @author: Nathan
+ * @author: Yujie
+ * @author: Viltene
+ * @version: last time updated on 25/08/23
+ * The game relies on JSFML
+ * This class runs the game 
+ */
+public class Driver {
+    public static void main(String[] args) 
+    {
+        MainPage main = new MainPage();
+    }
+}
